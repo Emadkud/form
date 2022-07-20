2e6de7b00d087d2e93b41df57983c9fb35022a33
Emad0007 <BASEMRF007@GMAIL.COM>
2022-07-20 02:36:26 +0100

    form

--- src/App.js
+++ src/App.js
@@ -1,52 +1,95 @@
-import React from 'react'
+import React, { Component } from "react";
+class App extends Component {
+  constructor(){
+    super()
 
-class App extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            email: "",
-            password: "",
-            rememberMe: false,
-            emailIsValid: false,
-            passwordIsValid: false,
-            isSubmitted: false
-        }
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false
     }
-    handelEmailChange = (e) => {
-        this.setState ({
-          
-            email:e.target.value},
-      () => {} 
-    
+  }
+
+  handleEmailChange = (e) => {
+    const value = e.target.value
+    // console.log (value)
+    const regEx = new RegExp(/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    const isValide = regEx.test(value)
+
+
+   this.setState({
+    email : value,
+    emailIsValid: isValide
+   })
+
+  }
+
+   handlePasswordChange = (e) => {
+    const value = e.target.value
+     const isValid = value.length > 5
      
-   
-      
-    )};
-     render(){
-        return(
-        
-           
-            <form onSubmit={this.handleSubmit} >
-            <div classeName="mb-3">
-              <label htmlfor="exampleInputEmail1" classeName="form-label">Email address</label>
-              <input type="email" classeName="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-              <div id="emailHelp" classeName="form-text" onChange={this.handleChange}></div>
-            </div>
-            <div classeName="mb-3">
-              <label htmlfor="exampleInputPassword1" classeName="form-label">Password</label>
-              <input type="password" classeName="form-control" id="exampleInputPassword1" onChange={this.handleChange}/>
-            </div>
-            <div classeName="mb-3 form-check">
-              <input type="checkbox" classeName="form-check-input" id="exampleCheck1" onChange={this.handleChange}/>
-              <label classeName="form-check-label" htmlfor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" classeName="btn btn-primary">Submit</button>
-          </form>
-          
-        )
-     }
+     this.setState({
+      password : value,
+      passwordIsValid: isValid,
 
+    })
+
+   }
+
+   handleRememberMeChange = (e) => {
+   this.setState({ rememberMe : e.target.checked })
+
+   } 
+
+   handleSubmit = (e) => {
+    e.preventDefault()
+    const allIsValid = this.state.passwordIsValid &&this.state.emailIsValid
+            this.setState({isSubmitted : allIsValid})
+    
+
+   }
+ 
+
+
+
+
+  render() {
+ 
+    return (
+      <>
+      <h1 className="text-center mt-5">login</h1>
+      {this.state.isSubmitted ? (
+        <div>
+          <p>{this.state.email}</p>
+        </div>
+      ) : (
+        <div className="container d-flex justify-content-center mt-5">
+     
+      <form className="col-6"  onSubmit={this.handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label"> Email address</label>
+            <input type="email" className={`form-control ${this.state.emailIsValid ? 'is-valid' : 'is-invalid' }`} id="email"  onChange={this.handleEmailChange} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Password" className="form-label"> Password</label>
+            <input type="password" className={`form-control ${this.state.passwordIsValid ? 'is-valid'  : 'is-invalid'}`} id="Password"  onChange ={this.handlePasswordChange} />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="remember-me"  onChange={this.handleRememberMeChange}  />
+            <label className="form-check-label" htmlFor="exampleCheck1"> Remember me</label>
+          </div>
+          <button type="submit" className="btn btn-primary" > Submit</button>
+      </form>
+    
+      </div>
+      )
+    }
+    
+      </>
+    );
+  }
 }
-
-export default App
+export default App;
